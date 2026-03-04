e2774e081728c024efb0ae0aa7a72d929762fbbd
bur :: Andrew <bur.and.by@gmail.com>
2026-03-04 20:07:20 +0300

    Update to support the newest HA

--- custom_components/ptz_camera/__init__.py
+++ custom_components/ptz_camera/__init__.py
@@ -1,182 +1,138 @@
-import requests
+import logging
 import time
 
+import requests
+
 DOMAIN = "ptz_camera"
+LOGGER = logging.getLogger(__name__)
 
 ATTR_HOST = "host"
 ATTR_PROFILE = "profile"
+ATTR_PAN_TIME = "pan_time"
+ATTR_MOVE_TIME = "move_time"
+
 DEFAULT_HOST = "192.168.1.244"
 DEFAULT_PROFILE = "Profile_1"
-ATTR_PAN_TIME = "pan_time"
-DEFAULT_PANT_TIME = 10
-ATTR_MOVE_TIME = "move_time"
+DEFAULT_PAN_TIME = 10
 DEFAULT_MOVE_TIME = 0.3
-DEFAULT_HEADERS = {'Content-Type':'application/soap+xml;charset=UTF8'}
+DEFAULT_HEADERS = {"Content-Type": "application/soap+xml;charset=UTF8"}
 
-def setup(hass, config):
-    
+
+def _post_soap(host, xml):
+    response = requests.post(
+        f"http://{host}/onvif/PTZ",
+        data=xml,
+        headers=DEFAULT_HEADERS,
+        timeout=5,
+    )
+    response.raise_for_status()
+
+
+def _stop_xml(profile):
+    return f"""<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope"
+ xmlns:tptz="http://www.onvif.org/ver20/ptz/wsdl">
+ <soap:Body>
+ <tptz:Stop>
+ <tptz:ProfileToken>{profile}</tptz:ProfileToken>
+ <tptz:PanTilt>true</tptz:PanTilt>
+ <tptz:Zoom>true</tptz:Zoom>
+ </tptz:Stop>
+ </soap:Body>
+</soap:Envelope>"""
+
+
+def _move_xml(profile, x, y):
+    return f"""<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope"
+ xmlns:tptz="http://www.onvif.org/ver20/ptz/wsdl"
+ xmlns:tt="http://www.onvif.org/ver10/schema">
+ <soap:Body>
+ <tptz:ContinuousMove>
+ <tptz:ProfileToken>{profile}</tptz:ProfileToken>
+ <tptz:Velocity>
+ <tt:PanTilt x="{x}" y="{y}"/>
+ <tt:Zoom x="1"/>
+ </tptz:Velocity>
+ </tptz:ContinuousMove>
+ </soap:Body>
+</soap:Envelope>"""
+
+
+def _register_services(hass):
     def stop(call):
         host = call.data.get(ATTR_HOST, DEFAULT_HOST)
         profile = call.data.get(ATTR_PROFILE, DEFAULT_PROFILE)
-
-        xml = """<?xml version="1.0" encoding="utf-8"?>
-        <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope"
-         xmlns:tptz="http://www.onvif.org/ver20/ptz/wsdl">
-         <soap:Body>
-         <tptz:Stop>
-         <tptz:ProfileToken>Profile_1</tptz:ProfileToken>
-         <tptz:PanTilt>true</tptz:PanTilt>
-         <tptz:Zoom>true</tptz:Zoom>
-         </tptz:Stop>
-         </soap:Body>
-        </soap:Envelope> """
-
-        r = requests.post('http://'+host+'/onvif/PTZ', data=xml, headers=DEFAULT_HEADERS)
+        try:
+            _post_soap(host, _stop_xml(profile))
+        except requests.RequestException as err:
+            LOGGER.error("PTZ stop failed for host %s: %s", host, err)
 
     def move_left(call):
         host = call.data.get(ATTR_HOST, DEFAULT_HOST)
         profile = call.data.get(ATTR_PROFILE, DEFAULT_PROFILE)
         move_time = call.data.get(ATTR_MOVE_TIME, DEFAULT_MOVE_TIME)
-
-        xml = """<?xml version="1.0" encoding="utf-8"?>
-        <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope"
-         xmlns:tptz="http://www.onvif.org/ver20/ptz/wsdl"
-         xmlns:tt="http://www.onvif.org/ver10/schema">
-         <soap:Body>
-         <tptz:ContinuousMove>
-         <tptz:ProfileToken>Profile_1</tptz:ProfileToken>
-         <tptz:Velocity>
-         <tt:PanTilt x="-0.5" y="0"/>
-         <tt:Zoom x="1"/>
-         </tptz:Velocity>
-         </tptz:ContinuousMove>
-         </soap:Body>
-        </soap:Envelope>"""
-
-        r = requests.post('http://'+host+'/onvif/PTZ', data=xml, headers=DEFAULT_HEADERS)
-        time.sleep(move_time)
-        stop(call)
+        try:
+            _post_soap(host, _move_xml(profile, "-0.5", "0"))
+            time.sleep(move_time)
+            stop(call)
+        except requests.RequestException as err:
+            LOGGER.error("PTZ move_left failed for host %s: %s", host, err)
 
     def move_right(call):
         host = call.data.get(ATTR_HOST, DEFAULT_HOST)
         profile = call.data.get(ATTR_PROFILE, DEFAULT_PROFILE)
         move_time = call.data.get(ATTR_MOVE_TIME, DEFAULT_MOVE_TIME)
+        try:
+            _post_soap(host, _move_xml(profile, "0.5", "0"))
+            time.sleep(move_time)
+            stop(call)
+        except requests.RequestException as err:
+            LOGGER.error("PTZ move_right failed for host %s: %s", host, err)
 
-        xml = """<?xml version="1.0" encoding="utf-8"?>
-        <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope"
-         xmlns:tptz="http://www.onvif.org/ver20/ptz/wsdl"
-         xmlns:tt="http://www.onvif.org/ver10/schema">
-         <soap:Body>
-         <tptz:ContinuousMove>
-         <tptz:ProfileToken>Profile_1</tptz:ProfileToken>
-         <tptz:Velocity>
-         <tt:PanTilt x="0.5" y="0"/>
-         <tt:Zoom x="1"/>
-         </tptz:Velocity>
-         </tptz:ContinuousMove>
-         </soap:Body>
-        </soap:Envelope>"""
-
-        r = requests.post('http://'+host+'/onvif/PTZ', data=xml, headers=DEFAULT_HEADERS)
-        time.sleep(move_time)
-        stop(call)
-   
     def move_up(call):
         host = call.data.get(ATTR_HOST, DEFAULT_HOST)
         profile = call.data.get(ATTR_PROFILE, DEFAULT_PROFILE)
         move_time = call.data.get(ATTR_MOVE_TIME, DEFAULT_MOVE_TIME)
-
-        xml = """<?xml version="1.0" encoding="utf-8"?>
-        <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope"
-         xmlns:tptz="http://www.onvif.org/ver20/ptz/wsdl"
-         xmlns:tt="http://www.onvif.org/ver10/schema">
-         <soap:Body>
-         <tptz:ContinuousMove>
-         <tptz:ProfileToken>Profile_1</tptz:ProfileToken>
-         <tptz:Velocity>
-         <tt:PanTilt x="0" y="0.5"/>
-         <tt:Zoom x="1"/>
-         </tptz:Velocity>
-         </tptz:ContinuousMove>
-         </soap:Body>
-        </soap:Envelope>"""
-
-        r = requests.post('http://'+host+'/onvif/PTZ', data=xml, headers=DEFAULT_HEADERS)
-        time.sleep(move_time)
-        stop(call)
+        try:
+            _post_soap(host, _move_xml(profile, "0", "0.5"))
+            time.sleep(move_time)
+            stop(call)
+        except requests.RequestException as err:
+            LOGGER.error("PTZ move_up failed for host %s: %s", host, err)
 
     def move_down(call):
         host = call.data.get(ATTR_HOST, DEFAULT_HOST)
         profile = call.data.get(ATTR_PROFILE, DEFAULT_PROFILE)
         move_time = call.data.get(ATTR_MOVE_TIME, DEFAULT_MOVE_TIME)
-
-        xml = """<?xml version="1.0" encoding="utf-8"?>
-        <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope"
-         xmlns:tptz="http://www.onvif.org/ver20/ptz/wsdl"
-         xmlns:tt="http://www.onvif.org/ver10/schema">
-         <soap:Body>
-         <tptz:ContinuousMove>
-         <tptz:ProfileToken>Profile_1</tptz:ProfileToken>
-         <tptz:Velocity>
-         <tt:PanTilt x="0" y="-0.5"/>
-         <tt:Zoom x="1"/>
-         </tptz:Velocity>
-         </tptz:ContinuousMove>
-         </soap:Body>
-        </soap:Envelope>"""
-
-        r = requests.post('http://'+host+'/onvif/PTZ', data=xml, headers=DEFAULT_HEADERS)
-        time.sleep(move_time)
-        stop(call)
+        try:
+            _post_soap(host, _move_xml(profile, "0", "-0.5"))
+            time.sleep(move_time)
+            stop(call)
+        except requests.RequestException as err:
+            LOGGER.error("PTZ move_down failed for host %s: %s", host, err)
 
     def move_origin_pan(call):
         host = call.data.get(ATTR_HOST, DEFAULT_HOST)
         profile = call.data.get(ATTR_PROFILE, DEFAULT_PROFILE)
-
-        xml = """<?xml version="1.0" encoding="utf-8"?>
-        <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope"
-         xmlns:tptz="http://www.onvif.org/ver20/ptz/wsdl"
-         xmlns:tt="http://www.onvif.org/ver10/schema">
-         <soap:Body>
-         <tptz:ContinuousMove>
-         <tptz:ProfileToken>Profile_1</tptz:ProfileToken>
-         <tptz:Velocity>
-         <tt:PanTilt x="-0.5" y="0"/>
-         <tt:Zoom x="1"/>
-         </tptz:Velocity>
-         </tptz:ContinuousMove>
-         </soap:Body>
-        </soap:Envelope>"""
-
-        r = requests.post('http://'+host+'/onvif/PTZ', data=xml, headers=DEFAULT_HEADERS)
+        try:
+            _post_soap(host, _move_xml(profile, "-0.5", "0"))
+        except requests.RequestException as err:
+            LOGGER.error("PTZ move_origin_pan failed for host %s: %s", host, err)
 
     def move_origin_tilt(call):
         host = call.data.get(ATTR_HOST, DEFAULT_HOST)
         profile = call.data.get(ATTR_PROFILE, DEFAULT_PROFILE)
-
-        xml = """<?xml version="1.0" encoding="utf-8"?>
-        <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope"
-         xmlns:tptz="http://www.onvif.org/ver20/ptz/wsdl"
-         xmlns:tt="http://www.onvif.org/ver10/schema">
-         <soap:Body>
-         <tptz:ContinuousMove>
-         <tptz:ProfileToken>Profile_1</tptz:ProfileToken>
-         <tptz:Velocity>
-         <tt:PanTilt x="0" y="-0.5"/>
-         <tt:Zoom x="1"/>
-         </tptz:Velocity>
-         </tptz:ContinuousMove>
-         </soap:Body>
-        </soap:Envelope>"""
-
-        r = requests.post('http://'+host+'/onvif/PTZ', data=xml, headers=DEFAULT_HEADERS)
+        try:
+            _post_soap(host, _move_xml(profile, "0", "-0.5"))
+        except requests.RequestException as err:
+            LOGGER.error("PTZ move_origin_tilt failed for host %s: %s", host, err)
 
     def move_origin(call):
-        pan_time = call.data.get(ATTR_PAN_TIME, DEFAULT_PANT_TIME)
+        pan_time = call.data.get(ATTR_PAN_TIME, DEFAULT_PAN_TIME)
         move_origin_pan(call)
         time.sleep(pan_time)
         move_origin_tilt(call)
-
 
     hass.services.register(DOMAIN, "move_left", move_left)
     hass.services.register(DOMAIN, "move_right", move_right)
@@ -187,5 +143,14 @@ def setup(hass, config):
     hass.services.register(DOMAIN, "move_origin_pan", move_origin_pan)
     hass.services.register(DOMAIN, "move_origin_tilt", move_origin_tilt)
 
-    # Return boolean to indicate that initialization was successfully.
+
+def setup(hass, config):
+    LOGGER.info("Setting up PTZ Camera integration")
+    _register_services(hass)
+    return True
+
+
+async def async_setup(hass, config):
+    LOGGER.info("Setting up PTZ Camera integration (async)")
+    _register_services(hass)
     return True
